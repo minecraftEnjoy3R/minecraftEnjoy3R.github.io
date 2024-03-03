c94cbc31af75b2d593eb0b2d0ce8bf09e37ecf92
minecraftEnjoy3R <limonad123123@inbox.ru>
2024-03-03 11:47:02 +0300

    added logo

---
+++ app/_components/Header.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div
+    className='
+    justify-corner
+    flex
+    '>
+        <Image src='/logo.svg'
+        alt='logo'
+        width={200}
+        height={85}
+        />
+    </div>
+  )
+}
+
+export default Header
